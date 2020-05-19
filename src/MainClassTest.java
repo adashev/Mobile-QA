0492cb239f8be995f71037ac83d8ff15f1d728d1
Dima <dimahelper@mail.ru>
2020-05-19 16:22:11 +0300

    Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,16 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+   MainClass main = new MainClass();
+
    @Test
    public void testGetLocalNumber() {
-      MainClass main = new MainClass();
-      Assert.assertTrue("метод getLocalNumber неверно рассчитывает сумму", main.getLocalNumber() == 12);
+      Assert.assertTrue("getLocalNumber() неверно рассчитывает сумму", main.getLocalNumber() == 12);
    }
 
    @Test
    public void testGetClassNumber() {
-      MainClass main = new MainClass();
-      Assert.assertTrue("метод getClassNumber возвращает число меньше 45", main.getClassNumber() > 45);
+      Assert.assertTrue("getClassNumber() возвращает число меньше 45", main.getClassNumber() > 45);
+   }
+
+   @Test
+   public void testGetClassString() {
+      Assert.assertTrue("getClassString() возвращает строку, не содержащую подстроку hello или Hello", main.getClassString().startsWith("hello")
+            || main.getClassString().startsWith("Hyllo"));
    }
 
 }
