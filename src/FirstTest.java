f15cd48772976f6c86001954e67db2a2165f5818
Dima <dimahelper@mail.ru>
2020-06-18 19:56:53 +0300

    01. CoreTestCase

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,45 +1,19 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-   private AppiumDriver driver;
-
-   @Before
-   public void setUp() throws Exception {
-      DesiredCapabilities capabilities = new DesiredCapabilities();
-      capabilities.setCapability("platformName", "Android");
-      capabilities.setCapability("deviceName", "AndroidTestDevice");
-      capabilities.setCapability("platformVersion", "8.0");
-      capabilities.setCapability("automationName", "Appium");
-      capabilities.setCapability("appPackage", "org.wikipedia");
-      capabilities.setCapability("appActivity", ".main.MainActivity");
-      capabilities.setCapability("app", "g:/Job/Selenium/2020/Mobile-QA/apks/org.wikipedia.apk");
-
-      driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-   }
-
-   @After
-   public void tearDown() {
-      driver.quit();
-   }
-
+public class FirstTest extends CoreTestCase {
    @Test
-   public void firstTest() {
+   public void testSearch() {
       waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"), "Cannot find Search Wikipedia", 5);
       WebElement searchSrcTextElement = waitForPresentTextSearch();
       searchSrcTextElement.sendKeys("Java");
@@ -162,7 +136,7 @@ public class FirstTest {
    }
 
    @Test
-   public void assertTitleEx6() {
+   public void testAssertTitleEx6() {
       waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/search_container']"), "Cannot find Search Wikipedia", 10);
       waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java", "Cannot find search field", 5);
       String articlesName = "Object-oriented programming language";
